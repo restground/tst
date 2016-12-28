6833ba7674d721d5204bc3e8bb83bdc2835be287
restground <restground@gmail.com>
2016-12-28 17:18:43 +0900

    푸터 - 대표자 성명 변경

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ class Footer extends Component {
       <div className="footer">
         <Navbar inverse fixedBottom>
           <p>인성문화연구소 | 경기도 고양시 일산동구 경의로 333, 515-2 | 031-902-7773 | <span className="copyNum">저작물등록번호:제C-2016-023325호</span> </p>
-          <p>사업자번호 122-86-31734 | 대표자 박재신 | COPYRIGHT &copy 2016 INSEONGCULTURE all rights reserved</p>
+          <p>사업자번호 122-86-31734 | 대표자 오지섭 | COPYRIGHT &copy 2016 INSEONGCULTURE all rights reserved</p>
         </Navbar>
       </div> 
     );
