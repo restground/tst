299ced17773c4039b5db03f4b1197e1ed81bbd61
restground <restground@gmail.com>
2017-01-02 17:23:28 +0900

    회원가입 페이지 / 로그인 페이지 수정 완료

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class Register extends Component {
   render() {
     return (
-      <div className="page_register component_container bg_ptrn">
+      <div className="page_register noscroll bg_ptrn">
         <div className="container">
-				  <div className="bg_white col-md-6">
+				  <div className="bg_white col-md-6 col-md-offset-3">
             <div className="row">
 						  <div className="img_logo"></div>
 						</div>
 							<div className="row">
-								<div className="col-md-8">
+								<div className="col-md-8 col-md-offset-2">
 									<form className="form-inline">
 										<div className="form-group">
 											<label className="sr-only" htmlFor="id">아이디</label>
@@ -32,14 +32,14 @@ class Register extends Component {
 											<label className="sr-only" htmlFor="confirm">비밀번호 확인</label>
 											<input type="text" className="form-control" id="confirm_password" placeholder="비밀번호를 다시 한번 입력해주세요" />
 										</div>
-										<div className="form-group">
+										<div className="form-group form_last">
 											<label className="sr-only" htmlFor="code">보안코드</label>
 											<input type="password" className="form-control" id="code" placeholder="보안코드를 입력해주세요" />
 										</div>
 										<p><span className="glyphicon glyphicon-question-sign" aria-hidden="true"></span> 
 											보안코드는 테스트할 학생들이 입력하게 될 코드번호입니다. <br /> &nbsp;&nbsp;&nbsp;&nbsp;단,계정비밀번호와는 다르게 입력해주세요.</p>
 										<div className="btn_signup">
-											<button type="submit" className="btn btn-block" value="submit" >가입 완료</button>
+											<button type="submit" className="btn btn-block" value="submit" >회원가입</button>
 										</div>
 									</form>
 								</div>
