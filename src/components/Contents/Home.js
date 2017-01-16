d152433f25e49afbc0e1c9727ab123b42545e913
nidormuto <skerton101@gmail.com>
2017-01-16 16:58:49 +0900

    결과지, 회사소개 수정 완료

--- src/components/Contents/Home.js
+++ src/components/Contents/Home.js
@@ -8,17 +8,8 @@ class Home extends Component {
         <Jumbotron className="home_main">
           <Grid>
             <div className="home_main_text">
-              <h3>TST PROGRAM</h3>
-              <h3>학습 성향 및 방법에 관한 검사</h3>
-              <p>
-                <Button
-                  className="btn_red" 
-                  bsStyle="default"
-                  bsSize="large"
-                  href="/test"
-                  target="_blank">
-                  테스트 시작하기
-                </Button>
+             <p>
+               
               </p>
             </div>
           </Grid>
